e92675180224002b6a7961fb52dfc909b0e534da
seyedmehdiarabi <seyedmehdiarabi@gmail.com>
2022-07-20 09:40:03 +0430

    Project-47-Card-Info

---
+++ Project-47-Card-Info/js/script.js
@@ -0,0 +1,8 @@
+const img = document.querySelector('img');
+const icon = document.querySelector('.icon');
+
+
+img.onclick = ()=>{
+    img.classList.toggle('active');
+    icon.classList.toggle('active');
+}
